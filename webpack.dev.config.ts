d1e286db3ba3f696c79763c974a4a64616f8b6dd
Vick Kumar <vick.kumar44@gmail.com>
2021-05-17 10:24:11 -0400

    added a url loader

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -1,76 +1,80 @@
-import path from "path";
-import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'path'
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 // import ModuleFederationPlugin from "webpack/lib/container/ModuleFederationPlugin";
 // import {dependencies as deps} from './package.json'
-import { Configuration as WebpackConfiguration } from "webpack";
-import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+import { Configuration as WebpackConfiguration } from 'webpack'
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server'
 interface Configuration extends WebpackConfiguration {
-  devServer?: WebpackDevServerConfiguration;
+    devServer?: WebpackDevServerConfiguration
 }
 
 const config: Configuration = {
-  mode: "development",
-  output: {
-    publicPath: "/",
-  },
-  entry: "./src/index.tsx",
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react",
-              "@babel/preset-typescript",
-            ],
-          },
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  plugins: [
-    // new ModuleFederationPlugin({
-    //   name: "Root",
-    //   filename: "remoteEntry.js",
-    //   remotes: {
-        
-    //   },
-    //   exposes: {
-        
-    //   },
-    //   shared: {
-    //     ...deps,
-    //     react: {
-    //       singleton: true,
-    //       requiredVersion: deps.react,
-    //     },
-    //     "react-dom": {
-    //       singleton: true,
-    //       requiredVersion: deps["react-dom"],
-    //     },
-    //   },
-    // }),
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "build"),
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-    hot: true
-  },
-};
+    mode: 'development',
+    output: {
+        publicPath: '/',
+    },
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        // new ModuleFederationPlugin({
+        //   name: "Root",
+        //   filename: "remoteEntry.js",
+        //   remotes: {
 
-export default config;
+        //   },
+        //   exposes: {
+
+        //   },
+        //   shared: {
+        //     ...deps,
+        //     react: {
+        //       singleton: true,
+        //       requiredVersion: deps.react,
+        //     },
+        //     "react-dom": {
+        //       singleton: true,
+        //       requiredVersion: deps["react-dom"],
+        //     },
+        //   },
+        // }),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        historyApiFallback: true,
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+}
+
+export default config
